cc5942f919178e47edddd5dbc55e4ea04561e387
nick farmer <nicholasf428@proton.me>
2026-01-31 12:45:14 -0500

    added data cleanup logic for csv

--- program/scripts/ingest_csv_to_pgdb.py
+++ program/scripts/ingest_csv_to_pgdb.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pg_connect import PostgresConnect
 
 
@@ -8,18 +9,23 @@ engine = pg_db.get_engine()
 
 decklist_file_path = 'D:/decklist csvs/archidekt/aloy_(i got nothin).csv'
 decklist_df = pd.read_csv(decklist_file_path)
+columns = decklist_df.columns.str.lower()
+decklist_df.columns = columns.str.replace(' ', '_')
+decklist_df = decklist_df.replace('----', np.nan)
 
-# print(decklist_df)
+# print(decklist_df['price'])
+
 try:
     decklist_df.to_sql(
         name="archidekt",
         con=engine,
         schema="raw_decklist",
-        if_exists="replace",
+        if_exists="append",
         index=False
     )
     print("successfully uploaded csv")
-except:
+except Exception as e:
     print("could not load csv")
+    print(e)
 
 pg_conn.close()
