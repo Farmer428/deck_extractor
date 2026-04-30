d9fababed921eb7f04d8d9f34e4ac0fbd38c44c0
nick farmer <nicholasf428@proton.me>
2026-04-30 14:20:30 -0400

    changes to ingest script to allow for ingestion of an entire directory of csvs, as opposed to just a single hard-coded one. Also added functionality to insert a new column for decklist name

--- program/scripts/ingest_csv_to_pgdb.py
+++ program/scripts/ingest_csv_to_pgdb.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
 from pg_connect import PostgresConnect
 
 
@@ -7,25 +8,42 @@ pg_db = PostgresConnect()
 pg_conn = pg_db.connect("MTG")
 engine = pg_db.get_engine()
 
-decklist_file_path = 'D:/decklist csvs/archidekt/aloy_(i got nothin).csv'
-decklist_df = pd.read_csv(decklist_file_path)
-columns = decklist_df.columns.str.lower()
-decklist_df.columns = columns.str.replace(' ', '_')
-decklist_df = decklist_df.replace('----', np.nan)
+decklists_directory = Path('D:/decklist csvs/archidekt/')
+decklist_file_paths = decklists_directory.iterdir()
 
-# print(decklist_df['price'])
+for decklist_path in decklist_file_paths:
 
-try:
-    decklist_df.to_sql(
-        name="archidekt",
-        con=engine,
-        schema="raw_decklist",
-        if_exists="append",
-        index=False
-    )
-    print("successfully uploaded csv")
-except Exception as e:
-    print("could not load csv")
-    print(e)
+    decklist_name = Path(decklist_path).stem
+    decklist_df = pd.read_csv(decklist_path)
+
+    # Normalize column names
+    columns = decklist_df.columns.str.lower()
+    decklist_df.columns = columns.str.replace(' ', '_')
+
+    # --- FIX: rename second "collector_number" to "collection_status" ---
+    cols = pd.Series(decklist_df.columns)
+
+    for dup in cols[cols.duplicated()].index:
+        if cols[dup] == "collector_number":
+            cols[dup] = "collection_status"
+
+    decklist_df.columns = cols
+
+    decklist_df = decklist_df.replace('----', np.nan)
+    decklist_df.insert(loc=1, column='deck_name', value=decklist_name)
+
+    try:
+        decklist_df.to_sql(
+            name="archidekt",
+            con=engine,
+            schema="raw_decklist",
+            if_exists="append",
+            index=False
+        )
+        print("successfully uploaded csv")
+
+    except Exception as e:
+        print("could not load csv")
+        print(e)
 
 pg_conn.close()
